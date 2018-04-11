5ed56928b08883fe04f76410db9bf5037c64f073
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-11 18:54:15 -0300

    (inc) Controller Admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resource :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
     get 'categories', to: 'categories#index'
+    get 'admins/index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
   devise_for :members
 
   root 'site/home#index'
