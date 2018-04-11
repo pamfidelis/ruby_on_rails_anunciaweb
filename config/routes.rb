72939c140aefd21bdf2a6dae1d04b5bffe47a408
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-11 13:34:27 -0300

    (inc) Rotas para Category

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
+  get 'backoffice', to: 'backoffice/dashboard#index'
+
   namespace :backoffice do
-    get 'categories/index'
+    resource :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
+    get 'categories', to: 'categories#index'
   end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  get 'backoffice', to: 'backoffice/dashboard#index'
-
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
-
   devise_for :admins
   devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'site/home#index'
 end
