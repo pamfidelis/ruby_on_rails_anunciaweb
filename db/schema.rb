c5ccc68f5b8d201c7ebd78ff2b68ccdbf0d9160d
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-25 19:52:25 -0300

    (fix) Member migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425224214) do
-
+ActiveRecord::Schema.define(version: 20180425224600) do
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,7 +65,7 @@ ActiveRecord::Schema.define(version: 20180425224214) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
