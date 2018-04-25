55ebbddc844e89d417d1df59648e383c4fbab79e
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-25 09:47:56 -0300

    (inc) Security policy

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,7 @@
 module Backoffice::AdminsHelper
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    Admin.roles.map { |key, value| OptionsForRoles.new(key,value) }
+  end
 end
