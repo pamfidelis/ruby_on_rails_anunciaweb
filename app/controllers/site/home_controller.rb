c0c0fe6fcb9411c091099fba9761d36f3272a94e
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-25 19:55:56 -0300

    (inc) SiteController

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,6 +1,4 @@
-class Site::HomeController < ApplicationController
-  layout 'site'
-
+class Site::HomeController < SiteController
   def index
     @categories = Category.order(:description)
     @ads = Ad.limit(6).order(created_at: :desc)
