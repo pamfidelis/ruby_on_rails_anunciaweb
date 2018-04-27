47a1c6d3babee775f8d1bdbea06a37e6701089d0
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-27 20:14:11 -0300

    (inc) Action update to ad

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,5 @@
 class Site::Profile::AdsController < Site::ProfileController
-  before_action :set_ad, only: [:edit]
+  before_action :set_ad, only: [:edit, :update]
 
   def index
     @ads = Ad.member_current(current_member)
@@ -8,9 +8,21 @@ class Site::Profile::AdsController < Site::ProfileController
   def edit
   end
 
+  def update
+    if @ad.update(params_ad)
+      redirect_to site_profile_ads_path, notice: "Anúncio atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   private
 
     def set_ad
       @ad = Ad.find(params[:id])
+    end
+
+    def params_ad
+      params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture)
     end
 end
