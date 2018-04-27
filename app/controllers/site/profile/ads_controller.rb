68da41ee46c09b80d89b517dd2828da5801b9e70
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-27 13:59:58 -0300

    (inc) Ads layout edit

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,16 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit]
+
   def index
     @ads = Ad.member_current(current_member)
   end
+
+  def edit
+  end
+
+  private
+
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
 end
