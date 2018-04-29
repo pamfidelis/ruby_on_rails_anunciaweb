edf35f1ad29a7c84384ecb92fb203ad9af9fb0c6
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-29 16:49:53 -0300

    (alt) Showing a specific ad

--- app/controllers/site/ad_details_controller.rb
+++ app/controllers/site/ad_details_controller.rb
@@ -1,4 +1,15 @@
 class Site::AdDetailsController < SiteController
+  before_action :set_ad, only: [:show]
+
   def show
   end
+
+  private
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
+
+    def params_ad
+      params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture, :finish_date)
+    end
 end
