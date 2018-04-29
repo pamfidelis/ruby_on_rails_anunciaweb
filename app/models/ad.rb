0c6bb2ac49e78fda595d26c3fe80df70f8031a42
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-29 11:13:14 -0300

    (inc) Action create to member's ad and configurations

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,8 +2,10 @@ class Ad < ApplicationRecord
   belongs_to :category
   belongs_to :member
 
+  validates_presence_of :title, :category, :price, :description, :picture
+
   scope :descending_order, -> (quantity = 6) { limit(quantity).order(created_at: :desc)}
-  scope :member_current, -> (member) { where(member: member)}
+  scope :member_current, -> (member) { where(member: member).order(created_at: :desc)}
 
   # gem paperclip
   has_attached_file :picture, styles: { medium: "320x150>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
