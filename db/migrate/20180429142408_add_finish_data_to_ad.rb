d573f39a3046a5dec555d9a630108ffe231f742a
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-04-29 11:30:38 -0300

    (inc) Column finish_date to Ad

---
+++ db/migrate/20180429142408_add_finish_data_to_ad.rb
@@ -0,0 +1,5 @@
+class AddFinishDataToAd < ActiveRecord::Migration[5.1]
+  def change
+    add_column :ads, :finish_date, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425224600) do
+ActiveRecord::Schema.define(version: 20180429142408) do
 
   create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180425224600) do
     t.string "picture_content_type"
     t.integer "picture_file_size"
     t.datetime "picture_updated_at"
+    t.date "finish_date"
     t.index ["category_id"], name: "index_ads_on_category_id"
     t.index ["member_id"], name: "index_ads_on_member_id"
   end
