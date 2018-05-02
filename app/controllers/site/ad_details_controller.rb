c3568089cd7ac9050fd4e0214849acb92050f039
Pâmela Fidelis <pamela_olivia@hotmail.com>
2018-05-02 15:27:06 -0300

    (fix) Link to ad detail

--- app/controllers/site/ad_details_controller.rb
+++ app/controllers/site/ad_details_controller.rb
@@ -1,17 +1,6 @@
 class Site::AdDetailsController < SiteController
-  before_action :set_ad, only: [:show]
-
   def show
     @ad = Ad.find(params[:id])
-    @categories = Category.order_by_description
+    @categories = Category.order_by_description(params[:description])
   end
-
-  private
-    def set_ad
-      @ad = Ad.find(params[:id])
-    end
-
-    def params_ad
-      params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture, :finish_date)
-    end
 end
