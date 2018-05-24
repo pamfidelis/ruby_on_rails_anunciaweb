f119fe39b10687029bfb9f90f8177a829f4f06c0
Pâmela Fidelis <p000pfidelis@prpservicos.com.br>
2018-05-24 19:58:26 -0300

    (alt) Routes after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :store_current_location, :unless => :devise_controller?
+
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -9,18 +11,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def layout_by_resource
-      if devise_controller? && resource_name == :admin
-        "backoffice_devise"
-      elsif devise_controller? && resource_name == :member
-        "site_devise"
-      else
-        "application"
-      end
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "backoffice_devise"
+    elsif devise_controller? && resource_name == :member
+      "site_devise"
+    else
+      "application"
     end
+  end
 
-    def user_not_authorized
-      flas[:alert] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
-    end
+  def user_not_authorized
+    flas[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  private
+
+  def store_corrent_location
+    store_location_for(:member, request.url)
+  end
 end
--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Members::SessionsController < Devise::SessionsController
   protected
     def after_sign_in_path_for(resource)
-      site_profile_dashboard_index_path
+      #site_profile_dashboard_index_path
+      stored_location
     end
 end
